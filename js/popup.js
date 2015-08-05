3e632f7d3b1672f1f689ac26d1b9241432ebd0df
Stefan Stoichev <stefan.stoichev@gmail.com>
2015-08-05 07:26:49 +0100

    starting logic

--- js/popup.js
+++ js/popup.js
@@ -1,89 +1,38 @@
-  documents = store.get('docs');
-  $("#search_list").append(documents);
-  $('ul#search_list>li').tsort('a',{attr:'title'});
-  
-  $('#search_input').fastLiveFilter('#search_list', { 
-          callback:function(total) {
-          $('#search_list').unhighlight();
-          searchTerm = $("#search_input").val();
-          if (searchTerm.length > 0) {
-              $('#search_list').highlight(searchTerm);
-          }
-      }
-  });
-  $("#search_input").on("search", function() {
-    if ($('#search_input').val() == "")
-      $('#search_input').val('').change();
-  });
-  
-  websiteAddress = document.getElementById("search_input");
-  websiteAddress.focus();
-  
-  $('#error').hide()
-  errors = store.get('error');
-  if(errors) {
-    $('#error').html(errors);
-    $('#error').show();
+var config = {
+  host: 'wb2-mar-p0002.systems.private',
+  isSecure: true
+}
+// chrome.windows.create({
+//   url: 'https://wb2-mar-p0002.systems.private/dataloadeditor/app/52fb9488-bdf8-4946-a3fb-0a00540befb1',
+//   state: 'maximized'
+// });
+
+
+qsocks.Connect(config).then(function(global) {
+  console.log('Connected!')
+  global.getDocList().then(function(apps) {
+    console.log(apps)
+    var table = "<table id='docListTable' class='tablesorter' width=100%><thead><tr><th>App</th><th>Modified</th><th>...</th> </tr></thead>"
+    for(var i = 0; i < apps.length; i++) {
+        var app = apps[i];
+        var modifiedDate = moment(app.qMeta.modifiedDate).format('YYYY-MM-DD hh:mm');
+        table += '<tr><td><a target="_newtab" href="https://wb2-mar-p0002.systems.private/sense/app/'+ app.qDocId + '">' + app.qDocName + '</a></td><td>' + modifiedDate + '</td><td>Delete</td></tr>';
+        //if(app.qDocName.indexOf('Automated') > -1) {
+          console.log(app.qDocName);
+        //}
   }
 
-  $(".btn-group > .btn").click(function(){
-      $(this).addClass("active").siblings().removeClass("active");
-  });
-  
-  $('#sortTitle').addClass("active");
-  $('#sortasc').addClass("active");
+  $('#docListDiv').append(table);
+  $("#docListTable").tablesorter();
 
-  sort = 'asc';
-  Field = 1;
-  Sort(1,sort)
-  
-  $("#sortTitle").live("click", function() {
-    Field = 1;
-    Sort(1, sort) 
-  }); 
-  
-  $("#sortServer").live("click", function() {
-    Field = 2;
-    Sort(2, sort) 
-  });   
-  
-  $("#sortLastModified").live("click", function() {
-    Field = 3;
-    Sort(3, sort)
-  });     
-  
-  $("#sortLastUpdated").live("click", function() {
-    Field = 4;
-    Sort(4, sort)    
-  });       
-  
-  $("#sortasc").live("click", function() {
-    sort = 'asc';
-    Sort(Field, sort);
-  });       
-  
-  $("#sortdesc").live("click", function() {
-    sort = 'desc';
-    Sort(Field, sort);
-  });       
-  
-  function Sort(field, order) {
-    switch(field) {
-        case 1:
-            $('ul#search_list>li').tsort('span[class=qvw]',{order:order});
-            break;
-        case 2:
-            $('ul#search_list>li').tsort('span.server','span.qvw',{order:order});
-            break;
-        case 3:
-            $('ul#search_list>li').tsort('span[class=lastmodified]',{order:order});
-            break;
-        case 4:
-            $('ul#search_list>li').tsort('span[class=lastupdate]',{order:order});
-            break;            
-        // default:
-        //     default code block
-    }    
-  }
+  })
+  // global.createApp('123').then(function(app) {
+  //   if(app.qSuccess == true) {
+  //     var appId = app.qAppId;
+  //     // https://wb2-mar-p0002.systems.private/dataloadeditor/app/52fb9488-bdf8-4946-a3fb-0a00540befb1
+  //   }
+  //});
 
-
+}).catch(function(error) {
+  console.log('oh no', error);
+});
